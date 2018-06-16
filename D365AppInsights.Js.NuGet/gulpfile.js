f23b5b4145a2c0c422317a1591a017ae1b71821a
Jason Lattimer <jason.lattimer@gmail.com>
2018-06-16 10:35:06 -0500

    Updated gulp build

--- D365AppInsights.Js.NuGet/gulpfile.js
+++ D365AppInsights.Js.NuGet/gulpfile.js
@@ -26,16 +26,16 @@ gulp.task("build:ts", function () {
     return tsProject.src().pipe(tsProject()).pipe(gulp.dest("../D365AppInsights.Js/js"));
 });
 
+gulp.task("move:ts", ["build:ts"], function () {
+    gulp.src("../D365AppInsights.Js/js/jlattimer.d365appinsights.d.ts").pipe(gulp.dest(paths.dTsDest));
+
+    gulp.src("../D365AppInsights.Js/ts/jlattimer.d365appinsights.ts").pipe(gulp.dest(paths.jsDest + "/ts"));
+});
+
 gulp.task("move:js", ["build:ts"], function () {
     gulp.src([paths.root + "../D365AppInsights.Js/scripts/AiLogger.js", paths.root + "../D365AppInsights.Js/js/jlattimer.d365appinsights.js"])
         .pipe(concat(paths.concatJsDest))
         .pipe(gulp.dest(""));
-});
-
-gulp.task("move:ts", ["build:ts"], function () {
-    gulp.src("../D365AppInsights.Js/ts/jlattimer.d365appinsights.ts").pipe(gulp.dest(paths.jsDest + "/ts"));
-    
-    gulp.src("../D365AppInsights.Js/js/jlattimer.d365appinsights.d.ts").pipe(gulp.dest(paths.dTsDest));
 });
 
 gulp.task("min:js", ["move:js"], function () {
